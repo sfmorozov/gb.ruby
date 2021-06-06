a1ebc030d6cfa991c4d42b0327680d5a7ee0ca81
sfmorozov <sfmorozov@gmail.com>
2021-06-06 13:00:57 +0500

    lesson2 excersises &rubocop try

---
+++ lanInventorу.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+class User
+  def fio
+    'Морозов Сергей Фёдорович'
+  end
+
+  def prof
+    'Руби-разработчик'
+  end
+end
+
+class Comp
+end
+
+class Lan
+end
+
+class Host
+end
+usr = User.new
+pc = Comp.new
+net = Lan.new
+hst = Host.new
+puts "ФИО: #{usr.fio}"
+puts "Профессия: #{usr.prof}"
+f = File.new('user.txt', 'w')
+f.puts "ФИО: #{usr.fio}"
+f.puts "Профессия: #{usr.prof}"
---
+++ test.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+puts 'Rubocop syntax test'
+
+# Userful stuff!
