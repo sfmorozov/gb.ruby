78322a121c3d807d18d0043adfe5c72bda03e760
sfmorozov <sfmorozov@gmail.com>
2021-06-15 12:55:17 +0500

    RE: delete some code for clean look

--- lesson4/random_colors.rb
+++ lesson4/random_colors.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 
 colors = %w[Красный Оранжевый Жёлтый Зелёный Голубой Синий Фиолетовый]
-puts colors[(rand(0..(colors.size - 1)))]
+puts colors[rand(colors.size - 1)]
