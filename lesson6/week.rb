99ddb81f604a529b574b72774e89c401ea9f5f36
sfmorozov <sfmorozov@gmail.com>
2021-06-18 11:16:23 +0500

    ex 1

---
+++ lesson6/week.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+MON = 'Понедельник'
+TUE = 'Вторник'
+WEN = 'Среда'
+THU = 'Четверг'
+FRI = 'Пятница'
+SAT = 'Суббота'
+SUN = 'Воскресенье'
+puts WEEK = [MON, TUE, WEN, THU, FRI, SAT, SUN].freeze
