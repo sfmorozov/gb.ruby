dd8cc8b8f007bcfe5f174a8d50b61e279989ece3
sfmorozov <sfmorozov@gmail.com>
2021-07-02 13:55:06 +0500

    ex5

--- lesson9/temperature.rb
+++ lesson9/temperature.rb
@@ -9,7 +9,7 @@ print 'Значение температуры? '
 value = gets.to_f
 case choose
 when 1
-  puts "#{value}C ==> #{cel2far(value)}F"
+  puts "#{value} C ==> #{cel2far(value)} F"
 when 2
-  puts "#{value}F ==> #{far2cel(value)}C"
+  puts "#{value} F ==> #{far2cel(value)} C"
 end
