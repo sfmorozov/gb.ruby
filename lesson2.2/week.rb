b0a4ab2c5b0f1b5c432b283423af5ff1651ab3d0
sfmorozov <sfmorozov@gmail.com>
2021-07-12 10:14:14 +0500

    ex5, ex6

--- lesson2.2/week.rb
+++ lesson2.2/week.rb
@@ -3,7 +3,7 @@
 require_relative 'lib/consts'
 
 print 'Дни недели на "С": '
-p WEEK.select { |x| x.start_with?('С') }
+p(WEEK.select { |x| x.start_with?('С') })
 
 print 'Короткий и длинный из месяцев '
 p MONTH.minmax_by(&:size)
