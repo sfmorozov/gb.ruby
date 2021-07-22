3fc415b1141e3518316c18a67ba571c7d8040b78
sfmorozov <sfmorozov@gmail.com>
2021-07-22 12:18:07 +0500

    ex2

---
+++ lesson2.5/lib/user.rb
@@ -0,0 +1,17 @@
+class User
+  attr_accessor :first_name, :last_name, :pathronymic
+
+  def initialize
+    print 'Введите имя пользователя (ФИО): '
+    fio = gets.chomp.split(' ').to_a
+    @first_name = fio[1]
+    @last_name = fio[0]
+    @pathronymic = fio[2]
+  end
+
+  def view
+    "#{@last_name} #{@first_name} #{@pathronymic}"
+  end
+
+end
+
