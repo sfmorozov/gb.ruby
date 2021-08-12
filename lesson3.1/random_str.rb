c62e363b8bf97d3d346eb1097001807eac562753
sfmorozov <sfmorozov@gmail.com>
2021-08-12 18:59:15 +0500

    ex1

--- lesson3.1/random_str.rb
+++ lesson3.1/random_str.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-arr = File.readlines(ARGV.first, chomp: true).sort_by!(&:size)
+arr = File.readlines('test.txt', chomp: true).sort_by!(&:size)
 puts "Случайная строка: #{arr[rand(0..arr.size - 1)]}"
 puts "Самая длинная строка: #{arr.last}"
 puts "Самая короткая строка: #{arr.first}"
